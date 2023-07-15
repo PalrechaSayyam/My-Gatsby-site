8cd07cec9b6d0b18b406a98bbcabeb71af8b19f9
Sayyam Palrecha <ee22btech11047@iith.ac.in>
2023-07-15 21:12:14 +0530

    made a basic header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,15 @@
 import * as React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
 import { 
-    container, 
-    heading,
+    container,
+    topWrapper,
     navLinks,
     navLinkItem,
     navLinkText,
-    siteTitle } from './layout.module.css'
+    siteTitle,
+    headerLogo,
+    logo} from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
 
@@ -21,20 +24,24 @@ const Layout = ({ pageTitle, children }) => {
   `)
 
   return (
-    <div className={container}>
-      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-      <nav>
-        <ul className={navLinks}>
-        <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
-        <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
-        <li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
-        </ul>
-      </nav>
-      <main>
-        <h1 className={heading}>{pageTitle}</h1>
-        {children}
-      </main>
-    </div>
+        <div className={topWrapper}>
+          <header>
+            <div className={container}>
+              <div className={headerLogo}>
+                <StaticImage className={logo} alt="Lambda Logo"
+                src="../images/lambda.jpg"
+                width={40}
+                height={40} />
+                <h1 className={siteTitle}>{data.site.siteMetadata.title}</h1>
+              </div>
+              <div className={navLinks}>
+                <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
+                <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+                <li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
+              </div>
+            </div>
+          </header>
+        </div>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,14 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
 import Seo from '../components/seo'
 
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <main>
-      <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      <StaticImage
-        alt="The Finance & Consulting Club of IIT Hyderabad"
-        src="../images/fcc_logo.png"
-      />
+    <Layout pageTitle="Home Page">
     </Layout>
-    </main>
   )
 }
 
